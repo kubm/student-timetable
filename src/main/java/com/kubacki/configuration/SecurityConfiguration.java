65a55206bfc677613ae078256d311b28b9041a17
kubm <nohero87@gmail.com>
2017-09-02 15:34:04 +0200

    added logout, prepared abstract domain class for dao authorization

--- src/main/java/com/kubacki/configuration/SecurityConfiguration.java
+++ src/main/java/com/kubacki/configuration/SecurityConfiguration.java
@@ -14,12 +14,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests().antMatchers("/console/**","/").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/login").permitAll()
-//                .and()
-//                .logout().permitAll()
+        httpSecurity.authorizeRequests().antMatchers("/console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll()
                      ;
 
         httpSecurity.csrf().disable();
